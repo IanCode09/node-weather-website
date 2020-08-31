b17f25bd5c3b11a4f0b8b1d59cf7f50ed1236c1d
IanCode09 <ianlombu@protonmail.com>
2020-08-31 15:56:26 +0700

    Added Humidity and Feelslike

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -23,7 +23,7 @@ const forecast = (latitude, longitude, callback) => {
         callback('Unable to find location', undefined)
     } 
     else {
-        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + '°C')
+        callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + '°C. It feels like : ' + body.current.feelslike + ' °C. The humidity is ' + body.current.humidity + ' %')
     }
 })
 }
